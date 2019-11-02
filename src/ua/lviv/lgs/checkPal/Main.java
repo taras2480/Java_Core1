35843f77c1bd03bd7b9567c9916d90baf3f9044e
taras2480 <aviator2480@gmail.com>
2019-11-03 01:40:11 +0200

    init 2 lesson 10hw

--- src/ua/lviv/lgs/checkPal/Main.java
+++ src/ua/lviv/lgs/checkPal/Main.java
@@ -2,8 +2,24 @@ package ua.lviv.lgs.checkPal;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NonFiveLessons {
+
+		String word = new String("ddddd");
+		word = word.toUpperCase();
+
+		if ((word.length() == 5) && (word.charAt(0) == word.charAt(4)) && (word.charAt(1) == word.charAt(3))) {
+
+			System.out.println("This is Palindrom");
+
+		} else if ((word.length() == 5) && ((word.charAt(0) != word.charAt(4)) || (word.charAt(1) != word.charAt(3)))) {
+			System.out.println("This is not Palindrom");
+
+		} else {
+
+			String message = "Not 5 letters";
+			throw new NonFiveLessons(message);
+
+		}
 
 	}
 
